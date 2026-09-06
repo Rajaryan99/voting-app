376f22c1739cdfdaba74522b620fe1ab268ef3d9
Rajaryan99 <raj.aryan2518@gmail.com>
2026-09-06 13:10:41 +0530

    express setup

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import 'dotenv/config'
+
+const app = express();
+
+const port = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+    res.send("Voting Application")
+})
+
+app.listen(port, () => {
+    console.log(`server is running on http://localhost:${port}`)
+})
